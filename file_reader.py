06542af079d0bfd39487e4617b98e383b2f4639a
changTiman <changtman@berkeley.edu>
2019-12-11 23:46:22 +0100

    file_reader

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,12 @@
+def test_format_data(filename):
+    data_file = open(filename, "r")
+    data = data_file.readline()
+    data = data.split() 
+    return [float(i) for i in data]
+
 def format_data(filename):
     data_file = open(filename, "r")
     data = data_file.readline()
-    data = data.split()
-    return [float(i) for i in data]
+    data = [float(i) for i in data.split()]
+    # format as [player_pos, adv_pos]
+    return [data[:2], data[2:4]]
