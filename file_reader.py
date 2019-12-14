ead625c0e0906eb40357ec680acce48267acc25e
changTiman <changtman@berkeley.edu>
2019-12-14 21:09:48 +0100

    nice

--- file_reader.py
+++ file_reader.py
@@ -1,12 +1,14 @@
+import numpy as np
+
 def test_format_data(filename):
     data_file = open(filename, "r")
     data = data_file.readline()
     data = data.split() 
-    return [float(i) for i in data]
+    return np.array([float(i) for i in data])
 
 def format_data(filename):
     data_file = open(filename, "r")
     data = data_file.readline()
     data = [float(i) for i in data.split()]
     # format as [player_pos, adv_pos]
-    return [data[:2], data[2:4]]
+    return np.array([data[:2], data[2:4]])
